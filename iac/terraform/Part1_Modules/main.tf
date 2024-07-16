b49cd2f0cc706f92f62562a97000f2fe99a09536
Boba Fett <mandalorianboba@hotmail.com>
2024-07-16 03:09:58 -0500

    hit the wall

--- iac/terraform/Part1_Modules/main.tf
+++ iac/terraform/Part1_Modules/main.tf
@@ -1,3 +1,14 @@
+locals {  
+  storageAccountNameEnv = substr("${var.storageAccountNameModule}${var.uniqueIdentifier}${random_string.random.id}",0,24)
+}
+
+resource "random_string" "random" {
+    length           = 10
+    special          = false
+    lower            = true
+    upper            = false 
+}
+
 resource "azurerm_resource_group" iac_rg {
   name     = var.resourceGroupName
   location = var.location
--- iac/terraform/Part1_Modules/variables.tf
+++ iac/terraform/Part1_Modules/variables.tf
@@ -8,3 +8,14 @@ variable "location" {
     nullable = false
     default = "East US"
 }
+
+variable "storageAccountName" {
+    type = string
+    nullable = false
+    default = "cmstgacct"
+
+    validation {
+      condition = length(var.storageAccountName) > 3
+      error_message = "The storage account name should be at least 3 characters"
+    }
+}
