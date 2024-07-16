f5c801fb5868b39ac157d740810f98f77133447d
Boba Fett <mandalorianboba@hotmail.com>
2024-07-16 00:36:14 -0500

    variables 1 fix

--- iac/terraform/Part1_Main_Module/main.tf
+++ iac/terraform/Part1_Main_Module/main.tf
@@ -15,9 +15,9 @@ variable "location" {
 } 
 
  resource "azurerm_storage_account" "iac_stg_acct" { 
-  name                     = "var.storageAccountName" 
-  resource_group_name      = "var.resourceGroupName" 
-  location                 = "var.location" 
+  name                     = var.storageAccountName
+  resource_group_name      = var.resourceGroupName 
+  location                 = var.location
   account_tier             = "Standard" 
   account_replication_type = "LRS" 
 }
