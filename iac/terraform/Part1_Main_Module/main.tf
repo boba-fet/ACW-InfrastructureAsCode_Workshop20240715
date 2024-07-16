4de93c2c6f30aad0443c31d084b4e19dd13aecb3
Boba Fett <mandalorianboba@hotmail.com>
2024-07-16 00:20:01 -0500

    providers

--- iac/terraform/Part1_Main_Module/main.tf
+++ iac/terraform/Part1_Main_Module/main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_version = ">=1.6.6"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"      
-    }    
-  }
-}
-
-provider "azurerm" {
-  features {    
-  }
-}
-
  resource "azurerm_storage_account" "iac_stg_acct" { 
   name                     = "iacstgacct20291231acw" 
   resource_group_name      = "iac-training-rg" 
