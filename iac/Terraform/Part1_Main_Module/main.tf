166dab29753afa233a337faeec55c7c2bcbb1c01
Boba Fett <mandalorianboba@hotmail.com>
2024-07-16 00:10:38 -0500

    push plan no tfstate

--- iac/Terraform/Part1_Main_Module/main.tf
+++ iac/Terraform/Part1_Main_Module/main.tf
@@ -13,3 +13,11 @@ provider "azurerm" {
   features {    
   }
 }
+
+ resource "azurerm_storage_account" "iac_stg_acct" { 
+  name                     = "iacstgacct20291231acw" 
+  resource_group_name      = "iac-training-rg" 
+  location                 = "eastus" 
+  account_tier             = "Standard" 
+  account_replication_type = "LRS" 
+}
