44f47199111c8476b7781f13803d6db451760d60
Boba Fett <mandalorianboba@hotmail.com>
2024-07-16 00:31:56 -0500

    updates

--- iac/Terraform/Part1_Main_Module/main.tf
+++ iac/Terraform/Part1_Main_Module/main.tf
@@ -1,7 +1,23 @@
+ variable "resourceGroupName" { 
+    type = string 
+    nullable = false 
+    default = "iac-training-rg" 
+} 
+variable "storageAccountName" { 
+    type = string 
+    nullable = false 
+    default = "iacstgacct20291231acw" 
+} 
+variable "location" { 
+    type = string 
+    nullable = false 
+    default = "eastus" 
+} 
+
  resource "azurerm_storage_account" "iac_stg_acct" { 
-  name                     = "iacstgacct20291231acw" 
-  resource_group_name      = "iac-training-rg" 
-  location                 = "eastus" 
+  name                     = "var.storageAccountName" 
+  resource_group_name      = "var.resourceGroupName" 
+  location                 = "var.location" 
   account_tier             = "Standard" 
   account_replication_type = "LRS" 
 }
